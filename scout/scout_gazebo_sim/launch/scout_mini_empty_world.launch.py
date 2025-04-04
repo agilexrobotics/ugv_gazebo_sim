b9919b65a030da055d8b3eb0368029a5d48ddb06
leaveyoustun <115580918+leaveyoustun@users.noreply.github.com>
2025-04-04 08:41:19 +0000

    add lidar to scout_mini

--- scout/scout_gazebo_sim/launch/scout_mini_empty_world.launch.py
+++ scout/scout_gazebo_sim/launch/scout_mini_empty_world.launch.py
@@ -152,7 +152,10 @@ def generate_launch_description():
   start_gazebo_server_cmd = IncludeLaunchDescription(
     PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
     condition=IfCondition(use_simulator),
-    # launch_arguments={'world': world}.items()
+    #launch_arguments={
+      #  'world': world,
+      #  'verbose': 'true'
+    #  }.items()
     )
  
   # Start Gazebo client    
